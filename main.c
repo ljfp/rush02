5aa9a9e9544fe08ed132103b4b83acf6e9f68f40
Eduardo Galindo Salazar <egalindo@car13s3.42barcelona.com>
2025-07-27 14:57:13 +0200

    Update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: egalindo <egalindo@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/27 11:17:05 by egalindo          #+#    #+#             */
-/*   Updated: 2025/07/27 11:55:09 by egalindo         ###   ########.fr       */
+/*   Updated: 2025/07/27 14:56:36 by egalindo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,21 @@ char	*ft_convert_args(int argc, char **argv);
 
 int	main(int argc, char **argv)
 {
-	char	*str;
+	char	*str_num;
+	char	*dict_name;
 
 	if (ft_check_args (argc, argv) == 1)
 	{
 		write (1, "Error\n", 6);
 		return (1);
 	}
-	str = ft_convert_args(argc, argv);
-	printf("%s", str);
+	str_num = ft_convert_args(argc, argv);
+	if(argc == 2)
+		dict_name="numbers.dict";
+	if(argc ==3)
+		dict_name=argv[1];
+	
+	printf("%s", str_num);
+	printf("%s", dict_name);
 	return (0);
 }
